38457102bd879e0786768dfe1da1c4940e05d36b
kuroneko2929292929 <142378190+kuroneko2929292929@users.noreply.github.com>
2023-12-04 20:08:01 +0700

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Astra
+ * @since 1.0.0
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+?>
+<?php astra_content_bottom(); ?>
+	</div> <!-- ast-container -->
+	</div><!-- #content -->
+<?php 
+	astra_content_after();
+		
+	astra_footer_before();
+		
+	astra_footer();
+		
+	astra_footer_after(); 
+?>
+	</div><!-- #page -->
+<?php 
+	astra_body_bottom();    
+	wp_footer(); 
+?>
+<div style="display:none;">
+<a href="http://bpprd.bungokab.go.id:8087/template/situs-slot777-pulsa/"></a>
+<a href="https://simtaru.karimunkab.go.id/js/-/masterslot/"></a>
+<a href="http://bpprd.bungokab.go.id:8087/template/products/wso-slot/"></a>
+<a href="https://simtaru.karimunkab.go.id/google/vietnam/"></a>
+<a href="https://absensi.bkpsdm.landakkab.go.id/storage/situs-slotplus777/"></a>
+<a href="http://bpprd.bungokab.go.id:8087/template/products/wso-slot/"></a>
+<a href="https://bki.uinmataram.ac.id/wp-includes/js/-/cheat/">https://bki.uinmataram.ac.id/wp-includes/js/-/cheat/</a>
+<a href="https://kejari-bantaeng.kejaksaan.go.id/-/spgacor/"></a>
+<a href="https://agribisnis.umpwr.ac.id/-/slotgaransiantikalah/"></a>
+<a href="http://bpprd.bungokab.go.id/assets/css/situs-slot-garansi/"></a>
+<a href="https://kejati-sumaterautara.kejaksaan.go.id/akunwsoslot/"></a>
+<a href="https://suratppg.uinmataram.ac.id/logs/mslot/"></a>
+<a href="https://kejari-bantaeng.kejaksaan.go.id/products/masterslot-gacor/"></a>
+<a href="https://kejari-bima.kejaksaan.go.id/wp-content/mastergacor/"></a>
+<a href="https://kejati-sumaterautara.kejaksaan.go.id/akunwsoslot/"></a>
+<a href="https://kejari-berau.kejaksaan.go.id/assets/akunprothailand/"></a>
+<a href="https://kejari-boalemo.kejaksaan.go.id/content/slot777mantapoke/"></a>
+<a href="https://kejati-sumaterautara.kejaksaan.go.id/akunwsoslot/"></a>
+<a href="https://kejari-bintan.kejaksaan.go.id/wp-content/pulsagacor/"></a>
+<a href="https://kejari-tual.kejaksaan.go.id/admin/wp-content/slotplusgacor/"></a>
+</div>
+	</body>
+</html>
